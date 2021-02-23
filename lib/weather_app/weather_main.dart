cdb0c3b7a7f30b9184b656eee27d961ae0cdc84f
Sheshank Srivastava <ambitious9350164795@gmail.com>
2021-02-23 11:16:02 +0530

    UI is done

--- lib/weather_app/weather_main.dart
+++ lib/weather_app/weather_main.dart
@@ -1,17 +1,68 @@
 import 'package:flutter/material.dart';
 
-class WeatherApp extends StatelessWidget {
+class WeatherApp extends StatefulWidget {
+  @override
+  _WeatherAppState createState() => _WeatherAppState();
+}
+
+class _WeatherAppState extends State<WeatherApp> {
+  int temperature = 0;
+  var location = 'India';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Container(
-        decoration:BoxDecoration(
+        decoration: BoxDecoration(
             image: DecorationImage(
-              image: AssetImage('images/clear.png'),
-              fit: BoxFit.cover
-            )
-        )
-
+                image: AssetImage('images/clear.png'), fit: BoxFit.cover)),
+        child: Scaffold(
+            backgroundColor: Colors.transparent,
+            body: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Column(
+                  children: [
+                    Center(
+                      child: Text(
+                        '${temperature.toString()} *C',
+                        style: TextStyle(color: Colors.white, fontSize: 60),
+                      ),
+                    ),
+                    Center(
+                      child: Text(
+                        location,
+                        style: TextStyle(color: Colors.white, fontSize: 60),
+                      ),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Container(
+                      width: 300,
+                      child: TextField(
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 25,
+                        ),
+                        decoration: InputDecoration(
+                            hintText: 'Search another location',
+                            hintStyle: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                            ),
+                            prefixIcon: Icon(
+                              Icons.search,
+                              color: Colors.white,
+                            )),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            )),
       ),
     );
   }
